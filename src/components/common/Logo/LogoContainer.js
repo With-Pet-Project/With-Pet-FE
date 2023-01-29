2610621d14eca04dae0d431cdd5a8ceb0761997c
CGH <tt878429@gmail.com>
2023-01-29 16:32:36 +0900

    :gift: Feat: common/Logo, common/UserInfo 추가

---
+++ src/components/common/Logo/LogoContainer.js
@@ -0,0 +1,15 @@
+import './LogoContainer.scss';
+
+function LogoContainer() {
+  return (
+    <div className="logo-container">
+      <div>logo</div>
+      <div>
+        <span>Withpet</span>
+      </div>
+      <div>Rect</div>
+    </div>
+  );
+}
+
+export default LogoContainer;
