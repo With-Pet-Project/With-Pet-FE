dbed92b48bbfa4ac5b18fa85dea7780e823e0c76
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-01-31 13:35:41 +0900

    :gift: Feat: react-query 초기 세팅 및 global querykey 세팅

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import GlobalStyle from './lib/styles/GlobalStyle';
 import RootRoute from './routes';
+import { queryClient } from './reactQuery/queryClient';
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <RootRoute />
-    </>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
---
+++ src/reactQuery/queryKyes.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// https://tanstack.com/query/v4/docs/react/community/lukemorales-query-key-factory
+import { createQueryKeyStore } from '@lukemorales/query-key-factory';
+
+export const queryKeys = createQueryKeyStore({});
