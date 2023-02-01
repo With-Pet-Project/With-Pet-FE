eac8b68d17399381936827d2e35aabf80f6bb533
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-02-01 21:30:10 +0900

    :gift: feat: 위도 경도 가져오기 기능 추가 및 에러 처리

--- src/components/Hospital/Hospital.js
+++ src/components/Hospital/Hospital.js
@@ -1,13 +1,39 @@
 import { useState } from 'react';
+import { ERROR_MESSAGE } from 'constants/errorMessage';
 // chrome://settings/content/location
 function Hospital() {
-  const [coords, setCoords] = useState([null, null]);
+  const [location, setLocation] = useState(null);
 
-  const getLocation = () => {};
+  const showLocation = ({ coords }) => {
+    setLocation([coords.latitude, coords.longitude]);
+    console.log(coords.latitude, coords.longitude);
+  };
 
-  if (!coords) {
-    getLocation();
-  }
+  const showError = error => {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        throw new Error(ERROR_MESSAGE.PERMISSION_DENIED); // 나중에 좌표 넣어줘서 처리해주기
+      case error.POSITION_UNAVAILABLE:
+        throw new Error(ERROR_MESSAGE.POSITION_UNAVAILABLE);
+      case error.TIMEOUT:
+        throw new Error(ERROR_MESSAGE.POSITION_UNAVAILABLE);
+      case error.UNKNOWN_ERROR:
+        throw new Error(ERROR_MESSAGE.UNKNOWN_ERROR);
+      default:
+    }
+  };
+
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      try {
+        navigator.geolocation.getCurrentPosition(showLocation, showError);
+      } catch (error) {
+        console.log(error); // 나중에 분리하기
+      }
+    }
+  };
+
+  if (!location) getLocation();
 
   return <section>Hospital</section>;
 }
