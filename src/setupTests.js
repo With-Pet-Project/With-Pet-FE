0aa03e364107d8eb3a371cb83c5ca80928b87438
CGH <tt878429@gmail.com>
2023-02-08 22:23:12 +0900

    :white_check_mark: Test: msw 디렉토리 수정

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,7 +4,7 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 // src/setupTests.js
-import { server } from './mocks/server';
+import { server } from './mockServer/server';
 // Establish API mocking before all tests.
 beforeAll(() => server.listen());
 
