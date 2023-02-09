4aca24786a6f9e6d2881a6934c77cd6ae5f98fdf
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-02-09 14:57:29 +0900

    :gift: feat: 모달창 및 소비내역 추가 form UI 구현

--- src/components/Account/AccountMain/AccountMain.js
+++ src/components/Account/AccountMain/AccountMain.js
@@ -1,18 +1,28 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import TotalAccountSection from '../TotalAccountSection/TotalAccountSection';
 import TodayAccountSection from '../TodayAccountSection/TodayAccountSection';
+import Modal from './Modal';
 import './AccountMain.scss';
 
 function AccountMain() {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const openModal = () => {
+    setIsOpenModal(true);
+    console.log(isOpenModal);
+  };
+
   return (
     <section className="account-container">
       <TotalAccountSection />
       <TodayAccountSection />
-      <button type="button" className="add-account-btn">
+      <button type="button" className="add-account-btn" onClick={openModal}>
         <FontAwesomeIcon icon={faPlus} size="1x" />
       </button>
+      <Modal isOpen={isOpenModal} setIsOpenModal={setIsOpenModal} />
     </section>
   );
 }
--- src/components/Account/TotalAccountSection/ConsumptionItem/ConsumptionItem.js
+++ src/components/Account/TotalAccountSection/ConsumptionItem/ConsumptionItem.js
@@ -5,7 +5,10 @@ function ConsumptionItem() {
     <li className="total-consumption-item">
       <div className="item">
         <span className="name">사료/간식</span>
-        <span className="price">10,000원</span>
+        <div className="price">
+          <span>10,000</span>
+          <span className="unit">원</span>
+        </div>
       </div>
       <div className="range-wrapper">
         <div className="inner" />
