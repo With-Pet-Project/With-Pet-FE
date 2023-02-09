202e3253d323101d42028539a8d9a133efedf67c
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-02-09 11:01:32 +0900

    :gift: feat: 오늘의 소비 UI 구현

--- src/components/Account/TodayAccountSection/TodayAccountSection.js
+++ src/components/Account/TodayAccountSection/TodayAccountSection.js
@@ -1,6 +1,74 @@
 import './TodayAccountSection.scss';
 
 function TodayAccountSection() {
-  return <div className="today-account">TodayAccountSection</div>;
+  return (
+    <div className="today-account">
+      <div className="title-wrapper">
+        <h2>오늘의 소비</h2>
+        <button type="button">편집</button>
+      </div>
+      <div className="content-wrapper">
+        <div className="today-total">
+          <span className="total-title">전체 소비</span>
+          <div className="total-price">
+            <span>100,000</span>
+            <span className="unit">원</span>
+          </div>
+        </div>
+        <ul className="today-consumption-list">
+          <li className="today-consumption-item">
+            <div className="item-title">
+              <div className="color" />
+              <span className="name">사료/간식</span>
+            </div>
+            <div className="item-price">
+              <span>0,000</span>
+              <span className="unit">원</span>
+            </div>
+          </li>
+          <li className="today-consumption-item">
+            <div className="item-title">
+              <div className="color" />
+              <span className="name">사료/간식</span>
+            </div>
+            <div className="item-price">
+              <span>0,000</span>
+              <span className="unit">원</span>
+            </div>
+          </li>
+          <li className="today-consumption-item">
+            <div className="item-title">
+              <div className="color" />
+              <span className="name">사료/간식</span>
+            </div>
+            <div className="item-price">
+              <span>0,000</span>
+              <span className="unit">원</span>
+            </div>
+          </li>
+          <li className="today-consumption-item">
+            <div className="item-title">
+              <div className="color" />
+              <span className="name">사료/간식</span>
+            </div>
+            <div className="item-price">
+              <span>0,000</span>
+              <span className="unit">원</span>
+            </div>
+          </li>
+          <li className="today-consumption-item">
+            <div className="item-title">
+              <div className="color" />
+              <span className="name">사료/간식</span>
+            </div>
+            <div className="item-price">
+              <span>0,000</span>
+              <span className="unit">원</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 export default TodayAccountSection;
