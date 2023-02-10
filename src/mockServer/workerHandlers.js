3f3713a718160e3d2ea6de4296047ccf6bc70bcb
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-02-10 13:50:51 +0900

    :bug: fix: 가계부 가져오기 JSON API 변경

--- src/mockServer/workerHandlers.js
+++ src/mockServer/workerHandlers.js
@@ -1,12 +1,12 @@
 import { rest } from 'msw';
-import AllConsumption from 'lib/mocks/account/AllConsumption.json';
+import getFebData from 'lib/mocks/account/getFebData.json';
 
 const todos = ['먹기', '자기', '놀기'];
 
 export const workerHandlers = [
   // 소비 내역 전체를 조회합니다.
   rest.get('/consumption', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(AllConsumption));
+    return res(ctx.status(200), ctx.json(getFebData));
   }),
 
   // 할일 추가
