c9b96059d00380fdfd598b24483b6bef02ef70e0
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-02-13 08:58:25 +0900

    :gift: feat: Calender 오늘의 날짜 bold 처리

--- src/components/common/Calender/DateBox.js
+++ src/components/common/Calender/DateBox.js
@@ -1,8 +1,13 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import './DateBox.scss';
+import { TODAY } from 'constants/date';
+import { getMonthYearDetails } from './hooks/date';
 
 function DateBox({ id, day, handleSelectDate, selectDate, data = [] }) {
   const showHasDataIcon = selectDate !== id && data.length > 0;
+  const { dateTime: today } = getMonthYearDetails(TODAY);
+  const isToday = selectDate !== id && today === id;
+
   return (
     <div
       role="button"
@@ -10,7 +15,13 @@ function DateBox({ id, day, handleSelectDate, selectDate, data = [] }) {
       className="item"
       onClick={() => handleSelectDate(id)}
     >
-      <div className={`day ${selectDate === id ? 'active' : ''}`}>{day}</div>
+      <div
+        className={`day ${selectDate === id ? 'hasDataFont' : ''} ${
+          isToday ? 'today' : ''
+        }`}
+      >
+        {day}
+      </div>
       <div className={`hasDataIcon ${showHasDataIcon ? 'show' : ''}`} />
     </div>
   );
