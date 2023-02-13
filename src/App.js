a2c49f72040a10f29ea4e6ad3d23681a13a52453
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-02-13 19:54:18 +0900

    :gift: Feat: 전역 로딩 구현
    useIsFetching함수 사용, 지금은 주석 처리함

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import GlobalStyle from './lib/styles/GlobalStyle';
 import RootRoute from './routes';
 import { queryClient } from './reactQuery/queryClient';
+import Loading from './components/common/Loading/Loading';
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <GlobalStyle />
+      <Loading />
       <RootRoute />
       <ReactQueryDevtools />
     </QueryClientProvider>
