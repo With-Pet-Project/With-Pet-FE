4d36cbc61541c7459666623c9bddc175919e0ed8
CGH <tt878429@gmail.com>
2023-02-14 12:12:48 +0900

    :white_check_mark: Test: test코드 작성

--- src/components/Community/test/Search.test.js
+++ src/components/Community/test/Search.test.js
@@ -1,8 +1,25 @@
-import { render } from 'react-dom';
+import { render } from 'lib/test-utils/test-utils';
 import { screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import UserEvent from '@testing-library/user-event';
 
 import Search from '../Search/Search';
 
-test('when I click Input in Search Form', () => {
-  render(<Search />);
+test('when I focus on Input in Search Form', async () => {
+  const user = UserEvent.setup();
+  render(<Search focus />, { wrapper: BrowserRouter });
+
+  const searchInput = screen.getByRole('textbox', {
+    name: '',
+  });
+
+  const nullHistoryHeader = screen.queryByRole('heading', {
+    name: '최근 검색어',
+  });
+
+  await user.click(searchInput);
+
+  const historyHeader = screen.getByRole('heading', {
+    name: '최근 검색어',
+  });
 });
