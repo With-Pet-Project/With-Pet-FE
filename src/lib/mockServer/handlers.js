929c7ad8e4703560c5f4a27b463df34ad4157a37
CGH <tt878429@gmail.com>
2023-02-15 18:51:32 +0900

    :bug: Fix:  병합 후 handler 오류 해결

--- src/lib/mockServer/handlers.js
+++ src/lib/mockServer/handlers.js
@@ -7,6 +7,11 @@ import {
   ALL_OF_CHALLENGES,
   ONE_CHALLENGE,
 } from 'lib/mocks/challenge/challengeGet';
+import {
+  ARTICLE_LIST_PAGE_ONE,
+  ARTICLE_LIST_PAGE_TWO,
+  ARTICLE_LIST_PAGE_THREE,
+} from 'lib/mocks/article/articleGet';
 
 export const handlers = [
   rest.get('/consumption/:year/:month', (req, res, ctx) => {
@@ -80,5 +85,25 @@ export const handlers = [
   /** ---------------- Challenge ----------------------- */
   rest.get(`${BASE_URL}/challenge`, (req, res, ctx) => {
     return res(ctx.json(ALL_OF_CHALLENGES));
+  }),
+
+  /** -------------------------------------------------- */
+  /** ---------------- Article ----------------------- */
+  rest.get(`${BASE_URL}/article`, (req, res, ctx) => {
+    const { pageNum } = req.params;
+
+    if (pageNum === 1) {
+      return res(ctx.json(ARTICLE_LIST_PAGE_ONE));
+    }
+
+    if (pageNum === 2) {
+      return res(ctx.json(ARTICLE_LIST_PAGE_TWO));
+    }
+
+    if (pageNum === 3) {
+      return res(ctx.json(ARTICLE_LIST_PAGE_THREE));
+    }
+
+    return res(ctx.json(ARTICLE_LIST_PAGE_ONE));
   }),
 ];
