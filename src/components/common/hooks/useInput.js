9d47ed1ea7c8a9a2b7561d7c7ef17c2d0b428801
CGH <tt878429@gmail.com>
2023-02-17 02:18:53 +0900

    :bug: Fix: useInput 수정

--- src/components/common/hooks/useInput.js
+++ src/components/common/hooks/useInput.js
@@ -2,12 +2,26 @@ import { useState } from 'react';
 
 export function useInput(validation = () => true) {
   const [value, setValue] = useState('');
+  const [focus, setFocus] = useState(false);
+  const [valid, setValid] = useState(false);
 
   const handleChange = e => {
     setValue(e.target.value);
   };
 
-  const isValid = validation(value);
+  const isFocus = () => setFocus(true);
+  const isBlur = () => setFocus(false);
 
-  return { value, setValue, handleChange, isValid };
+  const isValid = () => setValid(validation(value));
+
+  return {
+    value,
+    setValue,
+    handleChange,
+    focus,
+    isFocus,
+    isBlur,
+    valid,
+    isValid,
+  };
 }
