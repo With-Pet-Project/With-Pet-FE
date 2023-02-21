ac1cc4d1f26e7114631b6b6ad9c48bb528699dc0
CGH <tt878429@gmail.com>
2023-02-21 15:45:13 +0900

    :gift: Feat: 모달 추가 및 모달 버튼 분리

---
+++ src/components/Diary/Modal/ModalButtons.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+import { vars } from 'lib/styles/vars';
+import { useModal } from 'components/common/Modal/context/useModal';
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+
+  & button {
+    width: 90px;
+    height: 36px;
+    border: 1.5px solid ${vars.backgroundYellow};
+    border-radius: 10px;
+    margin: 0 7px;
+  }
+
+  & button:first-child {
+    background-color: #fff;
+
+    &:hover {
+      background-color: ${vars.backgroundYellow};
+      color: white;
+    }
+  }
+
+  & button:last-child {
+    background-color: ${vars.backgroundYellow};
+    color: #fff;
+
+    &:hover {
+      background-color: #f3d124;
+    }
+  }
+`;
+
+function ModalButtons({ Component }) {
+  const { closeModal } = useModal();
+
+  return (
+    <ButtonWrapper>
+      <button type="button" onClick={() => closeModal(Component)}>
+        취소
+      </button>
+      <button type="button">추가하기</button>
+    </ButtonWrapper>
+  );
+}
+
+export default ModalButtons;
