54e1a3fe69bdbaafeecf8c5f53b7c5d30c594bc1
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-02-21 21:36:51 +0900

    :gift: feat: 나의 게시글 UI 구현

--- src/components/auth/Profile/MyPostSection/MyPostSection.js
+++ src/components/auth/Profile/MyPostSection/MyPostSection.js
@@ -1,7 +1,40 @@
 import './MyPostSection.scss';
 
 function MyPostSection() {
-  return <div className="MyPostSection">MyPostSection</div>;
+  return (
+    <div className="MyPostSection">
+      <h2 className="mypost-title">나의 게시글</h2>
+      <ul className="mypost-list">
+        <li className="mypost">
+          <span className="date">2023년 2월 10일</span>
+          <p className="content">
+            오늘은 반려견 산책 올바르게 하는 방법에 대해 알아보려고 합니다!
+            <br />
+            우리의 반려견과 산책하기 전에 준비해야 될 것과 알아야 할 것이 정말
+            많은데요.
+          </p>
+        </li>
+        <li className="mypost">
+          <span className="date">2023년 2월 10일</span>
+          <p className="content">
+            오늘은 반려견 산책 올바르게 하는 방법에 대해 알아보려고 합니다!
+            <br />
+            우리의 반려견과 산책하기 전에 준비해야 될 것과 알아야 할 것이 정말
+            많은데요.
+          </p>
+        </li>
+        <li className="mypost">
+          <span className="date">2023년 2월 10일</span>
+          <p className="content">
+            오늘은 반려견 산책 올바르게 하는 방법에 대해 알아보려고 합니다!
+            <br />
+            우리의 반려견과 산책하기 전에 준비해야 될 것과 알아야 할 것이 정말
+            많은데요.
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default MyPostSection;
