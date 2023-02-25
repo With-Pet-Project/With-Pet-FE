b8ab86c07be639ae8f240d362a7ef4b80dc0f360
CGH <tt878429@gmail.com>
2023-02-25 18:19:10 +0900

    :gift: :Feat: useUser훅 추가

---
+++ src/components/auth/hooks/useUser.js
@@ -0,0 +1,21 @@
+/* eslint-disable camelcase */
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { getUserInfo } from 'lib/APIs/login';
+import { QUERY_KEY } from 'lib/reactQuery/queryKeys';
+import { toast } from 'react-toastify';
+import { TOAST_MESSAGE, TOAST_OPTION } from 'components/common/Toast/toast';
+
+export function useUser() {
+  const jwt_token = localStorage.getItem('jwt_token') || null;
+
+  const { data: userInfo } = useQuery({
+    queryKey: [QUERY_KEY.UserInfo, jwt_token],
+    queryFn: () => getUserInfo(jwt_token),
+    onError: () => {
+      toast.error(TOAST_OPTION, TOAST_MESSAGE.CANNOT_GET_DATA);
+    },
+    enabled: !!jwt_token,
+  });
+
+  return userInfo?.data?.data;
+}
