ed941cd511ed774526aeb5f295ad0656b24416b5
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-02-26 21:20:45 +0900

    :white_check_mark: test: 페이지 네비게이션 테스팅 추가

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   projectId: 'n3yy1e',
+  env: { BASE_URL: 'http://localhost:3000/' },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
