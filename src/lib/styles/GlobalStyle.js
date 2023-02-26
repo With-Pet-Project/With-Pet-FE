8c8e5834c872b411077ea4e5e844f6591d48c448
CGH <tt878429@gmail.com>
2023-02-26 17:28:50 +0900

    :bug: Fix: 폰트 적용 안되던 문제 재해결

--- src/lib/styles/GlobalStyle.js
+++ src/lib/styles/GlobalStyle.js
@@ -11,11 +11,23 @@ const GlobalStyle = createGlobalStyle`
 
   body * {
     box-sizing: border-box;
+    font-family: 'Pretendard Variable', Pretendard,
+      -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue',
+      'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans', 'Noto Sans KR',
+      'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   a:visited, a:link {
     text-decoration: none;
     color: inherit;
+  }
+
+  input:focus {
+    outline: none;
+    background-color: #fff;
   }
 
   button {
