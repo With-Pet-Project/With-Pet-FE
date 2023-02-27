d31567e6b212ee102c6cae5d9d9248d2d1524a29
CGH <tt878429@gmail.com>
2023-02-27 17:19:29 +0900

    :hammer: Refactor: useLogout, useUser 개선

--- src/components/auth/hooks/useUser.js
+++ src/components/auth/hooks/useUser.js
@@ -4,15 +4,21 @@ import { getUserInfo } from 'lib/APIs/login';
 import { QUERY_KEY } from 'lib/reactQuery/queryKeys';
 import { toast } from 'react-toastify';
 import { TOAST_MESSAGE, TOAST_OPTION } from 'components/common/Toast/toast';
+import { useNavigate } from 'react-router-dom';
+import { useLogout } from './useLogout';
 
 export function useUser() {
+  const navigate = useNavigate();
   const jwt_token = localStorage.getItem('jwt_token') || null;
+  const { logout } = useLogout();
 
   const { data: userInfo } = useQuery({
     queryKey: [QUERY_KEY.UserInfo, jwt_token],
     queryFn: () => getUserInfo(jwt_token),
     onError: () => {
-      toast.error(TOAST_OPTION, TOAST_MESSAGE.CANNOT_GET_DATA);
+      toast.error(TOAST_OPTION, TOAST_MESSAGE.LOGIN_FAIL);
+      logout();
+      navigate('/', { replace: true });
     },
     enabled: !!jwt_token,
   });
