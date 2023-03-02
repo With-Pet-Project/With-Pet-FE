575cd8766321547f115f3fb85c142ff8713185bf
CGH <tt878429@gmail.com>
2023-03-02 19:14:19 +0900

    :bug:fix: 카카오 로그인 KOE006에러 해결

--- src/lib/KakaoAPIs/client.js
+++ src/lib/KakaoAPIs/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://kauth.kakao.com';
-export const REDIRECT_URI = `${window.location.href}/oauth/callback`;
+export const REDIRECT_URI = `${process.env.REACT_APP_KAKAO_REDIRECT_URI}`;
 
 export const KAKAO_OAUTH_URL = `${BASE_URL}/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
