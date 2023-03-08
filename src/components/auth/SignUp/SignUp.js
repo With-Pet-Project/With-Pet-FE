54f209dfacd997bfde1f7a6a280288274bec5482
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-03-08 20:34:08 +0900

    :hammer: refector: 회원가입 유효성검사 리펙토링

--- src/components/auth/SignUp/SignUp.js
+++ src/components/auth/SignUp/SignUp.js
@@ -1,13 +1,14 @@
 /* eslint-disable consistent-return */
-import styled from 'styled-components';
 import { useRef, useState } from 'react';
-import '../common/auth.scss';
 import { useNavigate } from 'react-router-dom';
+import { ERROR_MESSAGE } from 'lib/constants/errorMessage';
 import { setSignup } from 'lib/APIs/signup';
 import { isValidateNickName } from 'lib/APIs/profile';
-import './SignUp.scss';
+
 import Input from '../common/Input/Input';
 import Container from '../common/Container/Container';
+import '../common/auth.scss';
+import './SignUp.scss';
 
 const CHECK_PASSWORD =
   /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
@@ -16,46 +17,63 @@ const CHECK_EMAIL =
 
 function SignUp() {
   const navigate = useNavigate();
+  const emailRef = useRef(null);
   const passwordCheckRef = useRef(null);
   const passwordRef = useRef(null);
   const nicknameRef = useRef(null);
   const [validPwd, setValidPwd] = useState(null);
   const [validNickname, setValidNickname] = useState(null);
+  const { VALIDATION } = ERROR_MESSAGE;
 
-  const handleSubmit = async event => {
-    event.preventDefault();
-    const { value: email } = event.target.email;
-    const { value: password } = event.target.password;
-    const { value: nickname } = event.target.nickname;
+  const validateValues = () => {
+    const { value: email } = emailRef.current;
+    const { value: password } = passwordRef.current;
+    const { value: nickname } = nicknameRef.current;
 
-    // 비밀번호와 비밀번호 확인과 일치하는지 검사
-    if (validPwd) return alert('비밀번호와 비밀번호 확인이 일치하지 않습니다.');
     console.log(email);
     console.log(password);
     console.log(nickname);
 
+    // 비밀번호와 비밀번호 확인과 일치하는지 검사
+    if (!validPwd) {
+      alert(VALIDATION.NOT_CORRECT_PASSWORD);
+      return false;
+    }
+
     // 이메일 유효성 검사
     if (!CHECK_EMAIL.test(email)) {
-      alert('아이디는 이메일 형식이어야 합니다.');
+      alert(VALIDATION.MALFUNCTION_ID);
       return false;
     }
 
     // 비밀번호 유효성 검사 (8글자 이상 문자 숫자 특수문자 포함)
     if (!CHECK_PASSWORD.test(password)) {
-      alert('비밀번호는 8글자 이상의 문자, 숫자, 특수문자를 포함해야합니다.');
+      alert(VALIDATION.MALFUNCTION_PWD);
       return false;
     }
 
     // 닉네임 존재하는지 검사
-    if (validPwd)
-      return alert('이미 존재하는 닉네임입니다. 다시 입력해주세요.');
+    if (!validNickname) {
+      alert(VALIDATION.NICKNAME_EXIST);
+      return false;
+    }
+    return true;
+  };
 
-    // const response = await setSignup(email, password, nickname);
+  const handleSubmit = async event => {
+    event.preventDefault();
+    const isValid = validateValues();
+    if (!isValid) return;
 
-    // if (response?.status === 200) {
-    //   alert('회원가입 되었습니다.');
-    //   navigate('/login');
-    // }
+    const { value: email } = emailRef.current;
+    const { value: password } = passwordRef.current;
+    const { value: nickname } = nicknameRef.current;
+
+    const response = await setSignup(email, password, nickname);
+    if (response?.status === 200) {
+      alert('회원가입 되었습니다.');
+      navigate('/login');
+    }
   };
 
   const handleCancel = () => {
@@ -65,14 +83,13 @@ function SignUp() {
   const isPasswordValid = () => {
     const { value: passwordValue } = passwordRef.current;
     const { value: passwordCheckValue } = passwordCheckRef.current;
-
-    // 비번확인이랑 비번이랑 같은지 확인
     if (passwordValue === passwordCheckValue) return setValidPwd(true);
     if (passwordCheckValue) return setValidPwd(false);
   };
 
   const isNicknameValid = async () => {
     const { value } = nicknameRef.current;
+    if (!value) return;
     const isValid = await isValidateNickName(value);
     setValidNickname(isValid);
   };
@@ -88,6 +105,7 @@ function SignUp() {
           type="text"
           id="email"
           name="email"
+          ref={emailRef}
           placeholder="이메일 형식으로 입력해주세요."
         />
         <label htmlFor="password" className="password">
@@ -99,7 +117,7 @@ function SignUp() {
           name="password"
           placeholder="8자이상의 문자, 숫자, 특수문자를 입력해주세요."
           ref={passwordRef}
-          onBlur={isPasswordValid}
+          onChange={isPasswordValid}
         />
         <label htmlFor="password-check" className="label">
           비밀번호 확인
@@ -109,7 +127,7 @@ function SignUp() {
           id="password-check"
           name="password-check"
           ref={passwordCheckRef}
-          onBlur={isPasswordValid}
+          onChange={isPasswordValid}
         />
         {validPwd === false && (
           <span className="validate">비밀번호가 일치하지 않습니다.</span>
