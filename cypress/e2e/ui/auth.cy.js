0a26540e819eb60e0dc317d943f4d0a4d1e81fa3
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-03-15 15:53:37 +0900

    :white:check:mark: test: auth cy dataset 추가

--- cypress/e2e/ui/auth.cy.js
+++ cypress/e2e/ui/auth.cy.js
@@ -26,45 +26,52 @@ describe('회원가입 및 로그인', () => {
     password: '1234',
   };
 
-  // it('회원가입을 한다.', () => {});
+  // it('회원가입을 한다.', () => {
+  //   cy.$('move-sign-up-btn').click();
+  //   cy.$('sign-up-id').type('cypress@test.com');
+  //   cy.$('sign-up-pwd').type('password123!');
+  //   cy.$('sign-up-pwd-check').type('password123!');
+  //   cy.$('sign-up-nickname').type('cypress new tester');
+  //   cy.$('sign-up-submit').click();
+  // });
 
-  // jwt 부분은 나중에...
-  it('로그인을 한다.', () => {
-    const user = ctx.users[0];
-    cy.log(user);
-    cy.login(user.id, user.password);
+  // // jwt 부분은 나중에...
+  // it('로그인을 한다.', () => {
+  //   const user = ctx.users[0];
+  //   cy.log(user);
+  //   cy.login(user.id, user.password);
 
-    cy.location('pathname').should('equal', '/');
-    cy.$('side-bar-hamburger').trigger('mouseover');
-    cy.$('side-ber-profile-img').should('have.attr', 'src', user.profileImg);
-    cy.$('side-ber-nickname').should('have.text', user.nickname);
-    cy.$('side-ber-id').should('have.text', user.id);
+  //   cy.location('pathname').should('equal', '/');
+  //   cy.$('side-bar-hamburger').trigger('mouseover');
+  //   cy.$('side-ber-profile-img').should('have.attr', 'src', user.profileImg);
+  //   cy.$('side-ber-nickname').should('have.text', user.nickname);
+  //   cy.$('side-ber-id').should('have.text', user.id);
 
-    cy.visit(`${Cypress.env('baseUrl')}`);
-    cy.$('move-login-btn').should('not.exist');
-  });
+  //   cy.visit(`${Cypress.env('baseUrl')}`);
+  //   cy.$('move-login-btn').should('not.exist');
+  // });
 
-  it('로그인시 잘못된 값을 입력하면 에러를 표출한다.', () => {
-    // 아이디와 비밀번호가 일치해야 한다.
-    cy.login('incorrectId', 'incorrectPwd');
-    // alert : 아이디 또는 비밀번호를 잘못 입력했습니다라고 뜬다 -> 나중에 toast로 바꾸기
-  });
+  // it('로그인시 잘못된 값을 입력하면 에러를 표출한다.', () => {
+  //   // 아이디와 비밀번호가 일치해야 한다.
+  //   cy.login('incorrectId', 'incorrectPwd');
+  //   // alert : 아이디 또는 비밀번호를 잘못 입력했습니다라고 뜬다 -> 나중에 toast로 바꾸기
+  // });
 
-  it('회원가입시 잘못된 값을 입력하면 에러를 표출한다.', () => {
-    cy.$('move-sign-up-btn').click();
-    cy.$('sign-up-id').type('cypress@test.com');
-    cy.$('sign-up-pwd').type('1234');
-    cy.$('sign-up-pwd-check').type('123');
-    cy.$('sign-up-nickname').type('dasom');
+  // it('회원가입시 잘못된 값을 입력하면 에러를 표출한다.', () => {
+  //   cy.$('move-sign-up-btn').click();
+  //   cy.$('sign-up-id').type('cypress@test.com');
+  //   cy.$('sign-up-pwd').type('1234');
+  //   cy.$('sign-up-pwd-check').type('123');
+  //   cy.$('sign-up-nickname').type('dasom');
 
-    cy.$('pwd-check-fail').should('exist');
-    cy.$('sign-up-pwd-check').type('4');
-    cy.$('pwd-check-fail').should('not.exist');
+  //   cy.$('pwd-check-fail').should('exist');
+  //   cy.$('sign-up-pwd-check').type('4');
+  //   cy.$('pwd-check-fail').should('not.exist');
 
-    cy.$('nickname-check-fail').should('exist');
-    cy.$('sign-up-nickname').clear();
-    cy.$('sign-up-nickname').type('cypress');
-    cy.$('sign-up-nickname').blur();
-    cy.$('nickname-check-fail').should('not.exist');
-  });
+  //   cy.$('nickname-check-fail').should('exist');
+  //   cy.$('sign-up-nickname').clear();
+  //   cy.$('sign-up-nickname').type('cypress');
+  //   cy.$('sign-up-nickname').blur();
+  //   cy.$('nickname-check-fail').should('not.exist');
+  // });
 });
