12f5b8ac6b7e6be47dad2cfa44b62c4bcfca41ed
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-03-15 10:26:06 +0900

    :white_check_mark: test: seeding 테스트 및 cy-data 적용

--- cypress.config.js
+++ cypress.config.js
@@ -1,36 +1,30 @@
 /* eslint-disable import/extensions */
 const { defineConfig } = require('cypress');
-const { seed } = require('./db-seeder');
+const { seed, filter } = require('./db-seeder');
 
 module.exports = defineConfig({
   projectId: 'n3yy1e',
-  // env: { baseUrl: 'http://localhost:3000/' },
-  env: { BASE_URL: 'https://with-pet-fe-cgh123198-with-pet-prod.vercel.app/' },
+  env: { baseUrl: 'http://localhost:3000/' },
+  // env: { baseUrl: 'https://with-pet-fe-cgh123198-with-pet-prod.vercel.app/' },
+  // env: { baseUrl: 'https://with-pet-fe.vercel.app/' },
   e2e: {
     setupNodeEvents(on, config) {
       on('task', {
         'db:seed': () => {
-          // const defaultSeed = { users: [] };
-          // const seedToUse = seeds || defaultSeed;
-          // seed(seedToUse);
-          // return null;
           seed();
           return null;
         },
-        // !: 여기 하는 중 ...
-        'filter:database': function (queryPayload) {
-          console.log(JSON.stringify(queryPayload));
-          // return queryDatabase(queryPayload, (data, attrs) =>
-          //   _.filter(data.results, attrs),
-          // );
-          return null;
+        'filter:database': queryPayload => {
+          const { entity } = queryPayload;
+          const filteredData = filter(entity);
+          return filteredData || null;
         },
-        'find:database': function (queryPayload) {
-          // return queryDatabase(queryPayload, (data, attrs) =>
-          //   _.find(data.results, attrs),
-          // );
-          return null;
-        },
+        // 'find:database': function (queryPayload) {
+        //   // return queryDatabase(queryPayload, (data, attrs) =>
+        //   //   _.find(data.results, attrs),
+        //   // );
+        //   return null;
+        // },
       });
       return config;
     },
--- db-seeder.js
+++ db-seeder.js
@@ -1,31 +1,30 @@
-// const { join, dirname } = require('node:path');
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const fs = require('fs');
 const path = require('path');
 
-// module.exports = {
-//   seed: async state => {
-//     const adapter = new FileSync('db.json');
-//     const db = low(adapter);
-//     db.setState(state).write();
-//   },
-// };
-
-const adapter = new FileSync('db.json');
+const databaseFile = path.join(
+  process.cwd(),
+  'src/lib/test-data',
+  'database.json',
+);
+const adapter = new FileSync(databaseFile);
 const db = low(adapter);
 
 module.exports = {
   seed: () => {
-    // const testSeed = JSON.parse(fs.readFileSync('database-seed.json'), 'utf-8');
     const testSeed = JSON.parse(
       fs.readFileSync(
-        path.join(process.cwd(), 'src/lib/data', 'database-seed.json'),
+        path.join(process.cwd(), 'src/lib/test-data', 'database-seed.json'),
         'utf-8',
       ),
     );
 
     db.setState(testSeed).write();
   },
-  filter: () => {},
+
+  filter: value => {
+    const users = db.get(value).value();
+    return users;
+  },
 };
