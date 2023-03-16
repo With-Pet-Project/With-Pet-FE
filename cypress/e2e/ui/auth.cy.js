0e745e79e220ae3040b62b58960387b5c18e29a6
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-03-16 15:56:17 +0900

    :white_check_mark: test: 토스트 표출 테스팅 추가

--- cypress/e2e/ui/auth.cy.js
+++ cypress/e2e/ui/auth.cy.js
@@ -1,3 +1,4 @@
+import { TOAST_MESSAGE } from '/src/components/common/Toast/toast';
 /// <reference types="Cypress"/>
 
 describe('회원가입 및 로그인', () => {
@@ -44,27 +45,26 @@ describe('회원가입 및 로그인', () => {
     cy.$('move-login-btn').should('not.exist');
   });
 
-  // it('로그인시 잘못된 값을 입력하면 에러를 표출한다.', () => {
-  //   // 아이디와 비밀번호가 일치해야 한다.
-  //   cy.login('incorrectId', 'incorrectPwd');
-  //   // alert : 아이디 또는 비밀번호를 잘못 입력했습니다라고 뜬다 -> 나중에 toast로 바꾸기
-  // });
+  it('로그인시 잘못된 값을 입력하면 에러를 표출한다.', () => {
+    cy.login('incorrectId', 'incorrectPwd');
+    cy.get('.Toastify').should('have.text', TOAST_MESSAGE.INCORRECT_ID_PWD);
+  });
 
-  //   it('회원가입시 잘못된 값을 입력하면 에러를 표출한다.', () => {
-  //     cy.$('move-sign-up-btn').click();
-  //     cy.$('sign-up-id').type('cypress@test.com');
-  //     cy.$('sign-up-pwd').type('1234');
-  //     cy.$('sign-up-pwd-check').type('123');
-  //     cy.$('sign-up-nickname').type('dasom');
+  it('회원가입시 잘못된 값을 입력하면 에러를 표출한다.', () => {
+    cy.$('move-sign-up-btn').click();
+    cy.$('sign-up-id').type('cypress@test.com');
+    cy.$('sign-up-pwd').type('1234');
+    cy.$('sign-up-pwd-check').type('123');
+    cy.$('sign-up-nickname').type('dasom');
 
-  //     cy.$('pwd-check-fail').should('exist');
-  //     cy.$('sign-up-pwd-check').type('4');
-  //     cy.$('pwd-check-fail').should('not.exist');
+    cy.$('pwd-check-fail').should('exist');
+    cy.$('sign-up-pwd-check').type('4');
+    cy.$('pwd-check-fail').should('not.exist');
 
-  //     cy.$('nickname-check-fail').should('exist');
-  //     cy.$('sign-up-nickname').clear();
-  //     cy.$('sign-up-nickname').type('cypress');
-  //     cy.$('sign-up-nickname').blur();
-  //     cy.$('nickname-check-fail').should('not.exist');
-  //   });
+    cy.$('nickname-check-fail').should('exist');
+    cy.$('sign-up-nickname').clear();
+    cy.$('sign-up-nickname').type('cypress');
+    cy.$('sign-up-nickname').blur();
+    cy.$('nickname-check-fail').should('not.exist');
+  });
 });
